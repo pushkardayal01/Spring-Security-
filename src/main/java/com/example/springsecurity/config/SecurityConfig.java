6c2e7f17927f9dbd194e42977296ae66e5ebadf3
Pushkar <pushkardayal07@gmail.com>
2024-08-15 13:14:45 +0530

    dabatase authentication done

--- src/main/java/com/example/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/example/springsecurity/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.springsecurity.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -11,7 +12,10 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 
 @Configuration
@@ -19,12 +23,18 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableMethodSecurity
 public class SecurityConfig {
 
+    @Autowired
+    DataSource dataSource;
+
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((requests) -> {
-            ((AuthorizeHttpRequestsConfigurer.AuthorizedUrl)requests.anyRequest()).authenticated();
+            ((AuthorizeHttpRequestsConfigurer.AuthorizedUrl)
+                    requests.requestMatchers("/h2-console/**").permitAll()
+                            .anyRequest()).authenticated();
+
         });
-        http.formLogin(Customizer.withDefaults());
+//        http.formLogin(Customizer.withDefaults());
         http.httpBasic(Customizer.withDefaults());
         return (SecurityFilterChain)http.build();
     }
@@ -33,9 +43,11 @@ public class SecurityConfig {
     @Bean
     public UserDetailsService users() {
         // The builder will ensure the passwords are encoded before saving in memory
+
+
         User.UserBuilder users = User.withDefaultPasswordEncoder();
         UserDetails user = users
-                .username("user")
+                .username("user1")
                 .password("password")
                 .roles("USER")
                 .build();
@@ -44,6 +56,11 @@ public class SecurityConfig {
                 .password("password")
                 .roles("ADMIN")
                 .build();
-        return new InMemoryUserDetailsManager(user, admin);
+
+        JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager(dataSource);
+        userDetailsManager.createUser(user);
+        userDetailsManager.createUser(admin);
+        return userDetailsManager;
+//        return new InMemoryUserDetailsManager(user, admin);
     }
 }
